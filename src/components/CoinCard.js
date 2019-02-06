bcd488d85de45e88171165b1b32f37e9a34a8314
Angelo James Arriola <angelojames.arriola@gmail.com>
2019-02-05 23:14:14 -0800

    styled upper row of coin card symb, name and price

--- src/components/CoinCard.js
+++ src/components/CoinCard.js
@@ -28,6 +28,26 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-around"
   },
+  coinSymbol: {
+    marginTop: 10,
+    marginLeft: 20,
+    marginRight: 5,
+    fontWeight: "bold",        
+  },
+  coinName: {
+    marginTop: 10,
+    marginLeft: 5,
+    marginRight: 20
+  },
+  seperator: {
+    marginTop: 10,
+  },
+  coinPrice: {
+    marginTop: 10,
+    marginLeft: "auto",
+    marginRight: 10,
+    fontWeight: "bold",        
+  },
   image: {
     width: 40,
     height: 40
@@ -60,13 +80,16 @@ const CoinCard = ({ symbol, coin_name, price_usd, percent_change_24h, percent_ch
           style={image}
           source={{ uri: images[symbol] }}
         />
-        <Text>{ symbol }</Text>
-        <Text>{ coin_name }</Text>
-        <Text>Price: <Text style={bold}>$</Text>{ price_usd.toFixed(2) }</Text>
+        <Text style={coinSymbol}>{symbol}</Text>
+        <Text style={seperator}>|</Text>
+        <Text style={coinName}>{coin_name}</Text>
+        <Text style={coinPrice}>
+          Price: <Text style={bold}>$</Text>{price_usd.toFixed(2)}
+        </Text>
       </View>
       <View style={statisticsContainer}>
-        <Text>Change (24h): { percent_change_24h.toFixed(2) }%</Text>
-        <Text>Change (7d): { percent_change_7d.toFixed(2) }%</Text>
+        <Text>Change (24h): {percent_change_24h.toFixed(2)}%</Text>
+        <Text>Change (7d): {percent_change_7d.toFixed(2)}%</Text>
       </View>
     </View>
   )
