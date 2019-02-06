fba1d61ab223dbebe4de0fd16eb63bbe0a2f1cfd
seintun <seintun.edu@gmail.com>
2019-02-05 16:30:56 -0800

    Created a Header file with App info

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Header = () => {
+  return(
+    <View>
+      <Text>
+          Cryptocurrency App, powered by AJ and Sein
+      </Text>
+    </View>
+  )
+};
+
+export { Header };
