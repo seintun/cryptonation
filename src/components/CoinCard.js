2dbfc6ea5ff7342c1827f756cf0bab995de07073
Angelo James Arriola <angelojames.arriola@gmail.com>
2019-02-05 22:06:17 -0800

    added styling to coin card component

--- src/components/CoinCard.js
+++ src/components/CoinCard.js
@@ -7,19 +7,33 @@ import {
 } from 'react-native';
 import { images } from '../Utils/CoinIcons';
 
-const CoinCard = () => {
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex'
+  },
+  image: {
+    width: 40,
+    height: 40
+  },
+  bold: {
+    fontWeight: 'bold'
+  }
+})
+
+const { container, image, bold } = styles;
+
+const CoinCard = ({ symbol, coin_name, price_usd, percent_change_24h, percent_change_7d }) => {
   return (
-    <View>
-      <Image />
-      <Text>
-        { props.symbol }
-      </Text>
-      <Text>
-        { props.price_usd }
-      </Text>
-      <Text>
-        { props.percent_change_24h }
-      </Text>
+    <View style={container}>
+      <Image
+        style={image}
+        source={{ uri: images[symbol] }}
+      />
+      <Text>{ symbol }</Text>
+      <Text>{ coin_name }</Text>
+      <Text><Text style={bold}>$</Text>{ price_usd }</Text>
+      <Text>Change past 24 hours: { percent_change_24h }</Text>
+      <Text>Change past 7 days: { percent_change_7d }</Text>
     </View>
   )
 };
