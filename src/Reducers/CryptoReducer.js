0523d150467451d81863f6334151b5c7707606b6
seintun <seintun.edu@gmail.com>
2019-02-05 21:11:02 -0800

    create Reducer

--- src/Reducers/CryptoReducer.js
+++ src/Reducers/CryptoReducer.js
@@ -1,3 +1,26 @@
+import { 
+  FETCHING_COIN_DATA,
+  FETCHING_COIN_DATA_SUCCESS, 
+  FETCHING_COIN_DATA_FAIL 
+} from '../Utils/ActionTypes';
+
+const initialState = {
+  isFetching: null,
+  data: [],
+  hasError: false,
+  errorMessage: null
+};
+
 export default function(state=[], action){
-  return state;
+  switch(action.type){
+    case FETCHING_COIN_DATA:
+      return Object.assign({}, state, {
+        isFetching: true,
+        data: null,
+        hasError: false,
+        errorMessage: null
+      })
+    default:
+      return state;
+  }
 }
