ae937c1d05b150dc192a70efaac917e362c9cdc5
Angelo James Arriola <angelojames.arriola@gmail.com>
2019-02-05 22:15:03 -0800

    passing data as props to coincard component

--- src/components/CryptoContainer.js
+++ src/components/CryptoContainer.js
@@ -2,15 +2,32 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { View, Text } from 'react-native';
 import FetchCoinData from '../Actions/FetchCoinData';
+import CoinCard from './CoinCard';
 
 class CryptoContainer extends Component {
   componentDidMount(){
     this.props.FetchCoinData();
   }
+
+  renderCoinCards() {
+    const { crypto } = this.props;
+
+    return crypto.data.map((coin, index) => 
+      <CoinCard
+        key={index}
+        coin_name={coin.name}
+        symbol={coin.symbol}
+        price_usd={coin.quote.USD.price}
+        percent_change_24h={coin.quote.USD.percent_change_24h}
+        percent_change_7d={coin.quote.USD.percent_change_7d}
+      />
+    )
+  }
+
   render() {
     return(
       <View>
-        <Text>CryptoContainer</Text>
+        {this.renderCoinCards()}
       </View>
     )
   }
